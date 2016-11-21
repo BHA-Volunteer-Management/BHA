695ce50ffe8009003b67991e992aa0f2d57fc199
Gregory Price <airman.price@gmail.com>
2016-11-21 01:13:55 -0500

    BHA-96: Migration tests.  Finish User and Assignment migrations

--- api/management/commands/accessmigration.py
+++ api/management/commands/accessmigration.py
@@ -3,7 +3,7 @@ import geocoder
 from django.core.management.base import BaseCommand
 from django.contrib.auth.models import User
 from django.utils import timezone
-from api.models import Volunteer, Contact, SEX_ENUM, VOLUNTEER_LEVEL_ENUM
+from api.models import Volunteer, Contact, Assignment, SEX_ENUM, VOLUNTEER_LEVEL_ENUM, LANGUAGE_ENUM
 
 
 # Create user object
@@ -13,7 +13,9 @@ def create_user(user_fields, i):
     try:
         user = User.objects.create_user(username=str(i) + user_fields['Email'],
                                         email=str(i) + user_fields['Email'],
-                                        password='')
+                                        password='',
+                                        first_name=user_fields['First Name'],
+                                        last_name=user_fields['Family Name'],)
     except Exception as e:
         return None
     return user
@@ -72,32 +74,75 @@ def create_volunteer(user, contact, volunteer_level, volunteer_fields):
                                          sex=sex,
                                          volunteer_level=volunteer_level,
                                          notes=volunteer_fields['Notes'],
-                                         inactive=not bool(volunteer_fields['Active?']))
+                                         inactive= (volunteer_fields['Active?'] != "TRUE"))
     volunteer.save()
     return volunteer
 
 
+def create_assignment(assignment_fields):
+    
+    contact = Contact.objects.create(street=assignment_fields["street"],
+                                     city=assignment_fields["city"],
+                                     state=assignment_fields["state"],
+                                     zip=assignment_fields["zip"],
+                                     phone_number=assignment_fields['phone'],
+                                     email="",
+                                     carrier=0,
+                                     preferred_contact=0)
+
+    date = timezone.datetime.strptime(assignment_fields["start_date"], "%m/%d/%y %H:%M:%S")
+    volunteer = Volunteer.objects.all()[:1].get()
+    lang = LANGUAGE_ENUM[int(assignment_fields["language"])][0]
+    assignment = Assignment.objects.create(name=assignment_fields["name"], 
+                                            posted_by=volunteer,
+                                            language_name=lang,
+                                            start_date=date,
+                                            contact=contact,
+                                            type=0,
+                                            notes=assignment_fields["notes"],
+                                            admin_notes=assignment_fields["admin_notes"],
+                                            status=0,
+                                            duration=float(assignment_fields["duration"]))
+    contact.save()
+    assignment.save()
+
+    return assignment
+
+
 def import_volunteers_from_file(file):
     # Fields necessary for mapping to respective objects
-    model_fields = {'user': ['Email'],
+    model_fields = {'user': ['Email', "First Name", "Family Name", "Active?"],
                     'volunteer': ['First Name', 'Family Name', 'Middle Name', 'Sex', 'Notes', 'Active?'],
                     'volunteer_level': ['training', 'Observation1', 'Observation2'],
-                    'contact': ['Address', 'Phone', 'Email']}
+                    'contact': ['Address', 'Phone', 'Email'],}
     
     with open(file) as csvfile:
         reader = csv.DictReader(csvfile)
-
         for i, row in enumerate(reader):
             fields_dict = {fields: {key: row[key] for key in model_fields[fields]} for fields in model_fields}
-
             try:
                 user = create_user(fields_dict['user'], i)
                 contact = create_contact(fields_dict['contact'])
                 level = get_volunteer_level(fields_dict['volunteer_level'])
-
                 volunteer = create_volunteer(user, contact, level, fields_dict['volunteer'])
             except Exception as e:
                 print("Couldn't import volunteer " + str(i) + "\n" + str(e))
+
+
+def import_assignments_from_file(file):
+    model_fields = {'assignment':["name", "posted_by", "volunteers", "language", "start_date", 
+                                    "time", "type", "notes", "admin_notes", "duration", "street",
+                                    "city", "state", "zip", "phone"]} 
+    with open(file) as csvfile:
+        reader = csv.DictReader(csvfile)
+        for i, row in enumerate(reader):
+            fields_dict = {fields: {key: row[key] for key in model_fields[fields]} for fields in model_fields}
+            #try:
+            assignment = create_assignment(fields_dict['assignment'])
+            #except Exception as e:
+            #    print("Couldn't import assignment " + str(i) + "\n" + str(e))
+    
+    return
 
 
 class Command(BaseCommand):
@@ -105,6 +150,8 @@ class Command(BaseCommand):
         parser.add_argument('file', nargs='+', type=str)
 
     def handle(self, *args, **options):
-        for file in options['file']:
-            import_volunteers_from_file(file)
+        if len(options['file']) > 0:
+            import_volunteers_from_file(options['file'][0])
+        if len(options['file']) > 1:
+            import_assignments_from_file(options['file'][1])
 
