19ad8fa5a0957cc2ce2f0a27d23c2632f5e55b7b
miranda <mchiu@ccs.neu.edu>
2016-11-29 18:11:47 -0500

    added icons to footer

--- js/client/app/common/footer/footer.controller.js
+++ js/client/app/common/footer/footer.controller.js
@@ -1,6 +1,13 @@
 class FooterController {
   constructor() {
     this.name = 'footer';
+    this.icon1 = require('./icon_1.png');
+    this.icon2 = require('./icon_2.png');
+    this.icon3 = require('./icon_3.png');
+    this.fb = require('./fb_icon.png');
+    this.flickr = require('./flickr.png');
+    this.cc = require('./ccicon.png');
+    this.blog = require('./blogicon.png');
   }
 }
 
