b9fb740b67610a14e4314c7ad3f5480697a903cf
Gregory Price <airman.price@gmail.com>
2016-12-01 19:30:32 -0800

    BHA-96: broken test

--- tests/test_database_migration.py
+++ tests/test_database_migration.py
@@ -159,7 +159,9 @@ class DBMigrationTests(TestCase):
     def test_assignment_info(self, **kwargs):
         for assignment in self.assignments:
             date = timezone.datetime.strptime(assignment["start_date"], "%m/%d/%y %H:%M:%S")
-            lang = LANGUAGE_ENUM[int(assignment["language"])][0]
+						langs = list(LANGUAGE_ENUM.keys())
+						langs.sort()
+						lang = lang[int(assignment_Fields["language"])]
             self.assertTrue(Assignment.objects.filter(name=assignment["name"],
                             language_name=lang,
                             start_date=date,
